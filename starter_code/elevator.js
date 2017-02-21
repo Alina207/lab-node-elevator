786995aa201afdb50c8618272ccac8f432829eb2
Alina Gasperino <alina.gasperino@gmail.com>
2017-02-21 09:22:23 -0500

    Incomplete

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,53 @@
-class Elevator {
+class Elevator { // For the logic of the elevator: accepting requests, moving up or down, etc.
   constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+    this.direction = "up";
+    this.currentFloor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    var clear = setInterval(() => this.update(), 1000);
+  } // should start a `setInterval` call the `update` function every second
+
+  stop() {
+    clearInterval(clear);
+  } // should stop the elevator's `setInterval` listening for requests
+
+  update() {
+    this.log(); // should (for now) display the current status of the elevator by calling the `log` function.
+  }
+  log() { // should print the info related to position & direction
+    console.log('Direction:' + this.direction + ' | Current Floor: ' + this.currentFloor);
+  }
+
+  floorUp() { // update the current floor by incrementing it by one. (But not above the top floor.)
+    this.direction = "up";
+    if (this.currentFloor < this.MAXFLOOR) {
+      this.currentFloor +=1;
+    } else {
+      console.log("This is the top floor");
+    }
+    this.update();
+  }
+
+  floorDown() { // update the current floor by subtracting one from the current floor. (But not below the ground floor.)
+    this.direction = "down";
+    if (this.currentFloor > 0) {
+      this.currentFloor -=1;
+    } else {
+      console.log("This is the bottom floor");
+    }
+    this.update();
+  }
+
   _passengersEnter() { }
+
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+
+  call() {
+    this.requests[Person]; // what happens when a person ‘calls’ the elevator; should receive a person object & add it as a request into elevator’s queue.
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,43 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+const myElevator = new Elevator();
+
+const Alina = new Person('Alina', '1', '2');
+const Cynthia = new Person('Cynthia', '2', '3');
+const Tommy = new Person('Tommy', '3', '4');
+const Chuckie = new Person('Chuckie', '4', '5');
+const Angelica = new Person('Angelica', '5', '6');
+const Stew = new Person('Stew', '6', '7');
+
+myElevator.floorUp(); // 1
+myElevator.floorUp(); // 2
+myElevator.floorUp(); // 3
+myElevator.floorUp(); // 4
+myElevator.floorUp(); // 5
+myElevator.floorUp(); // 6
+myElevator.floorUp(); // 7
+myElevator.floorUp(); // 8
+myElevator.floorUp(); // 9
+myElevator.floorUp(); // 10
+myElevator.floorUp(); // 11
+myElevator.floorDown(); // 10
+myElevator.floorDown(); // 9
+myElevator.floorDown(); // 8
+myElevator.floorDown(); // 7
+myElevator.floorDown(); // 6
+myElevator.floorDown(); // 5
+myElevator.floorDown(); // 4
+myElevator.floorDown(); // 3
+myElevator.floorDown(); // 2
+myElevator.floorDown(); // 1
+myElevator.floorDown(); // 0
+
+
+/*
+myElevator.call(Alina);
+myElevator.call(Cythia);
+myElevator.call(Tommy);
+myElevator.call(Chuckie);
+myElevator.call(Angelica);
+myElevator.call(Stew); */
